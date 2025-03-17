109c02b0640e934396cdbcb2941656fa9dfc8237
Maki Spariva <makspariva@gmail.com>
2025-03-17 13:53:55 +0100

    Add project files.

---
+++ Models/Coche.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetCarsMaki.Models
+{
+    public class Coche
+    {
+        public int IdCoche { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
---
+++ Repositories/RepositoryCoches.cs
@@ -0,0 +1,39 @@
+﻿using NugetCarsMaki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NugetCarsMaki.Repositories
+{
+    public class RepositoryCoches
+    {
+        private XDocument document;
+
+
+        public RepositoryCoches() {
+            string resourceName = "NugetCarsPGS.coches.xml";
+            Stream stream = this.GetType().Assembly.GetManifestResourceStream(resourceName);
+            this.document = XDocument.Load(stream);
+        }
+
+        public List<Coche> GetCoches() {
+            var consulta = from datos in this.document.Descendants("coche") select datos;
+
+            List<Coche> cars = new List<Coche>();
+            foreach (var tag in consulta) {
+                Coche coche = new Coche();
+                coche.IdCoche = int.Parse(tag.Element("idcoche").Value);
+                coche.Marca = tag.Element("marca").Value;
+                coche.Modelo = tag.Element("modelo").Value;
+                coche.Imagen = tag.Element("imagen").Value;
+
+                cars.Add(coche);
+            }
+            return cars;
+        }
+
+    }
+}
